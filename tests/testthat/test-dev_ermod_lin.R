9ccc1c9a4ee4f0c85e4c27577f5acabf16b38011
jgabry <jgabry@gmail.com>
2025-10-17 16:00:54 -0600

    Update test to avoid error due to change in loo_compare()
    The output of loo_compare() is now a data frame instead of a matrix.

--- tests/testthat/test-dev_ermod_lin.R
+++ tests/testthat/test-dev_ermod_lin.R
@@ -210,8 +210,9 @@ mod_mtcars <- dev_ermod_bin(
 test_that("Exposure metrics selection", {
   expect_equal(extract_var_exposure(ermod_bin_exp_sel), "AUCss_1000")
   expect_equal(
-    extract_exp_sel_comp(ermod_bin_exp_sel)[, 1],
-    c(AUCss_1000 = 0.000000, Cmaxss = -0.6490312, Cminss = -2.7563957)
+    extract_exp_sel_comp(ermod_bin_exp_sel)[, 1, drop = FALSE],
+    data.frame(elpd_diff = c(AUCss_1000 = 0.000000, Cmaxss = -0.6490312, Cminss = -2.7563957)),
+    ignore_attr = TRUE
   )
 
   priors <- prior_summary(ermod_bin_exp_sel$l_mod_exposures[[2]])
